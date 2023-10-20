1abc5a6602c717c20068ca01099b57e171d0bbe7
Eloisa Paixão <114162946+eloisapaixao@users.noreply.github.com>
2023-10-20 11:26:19 -0300

    Atualização 20/10/2023 Erro em cadastrar!

--- CrossCare-Nursing-Edition/app/src/main/java/com/example/crosscare_nursing_edition/CadastroActivity.kt
+++ CrossCare-Nursing-Edition/app/src/main/java/com/example/crosscare_nursing_edition/CadastroActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
 import android.view.WindowManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class CadastroActivity : AppCompatActivity() {
+    val db = Firebase.firestore
+
+    lateinit var btnEntrar: Button
+    lateinit var edtNome: EditText
+    lateinit var edtSenha: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.cadastrar)
@@ -19,5 +31,32 @@ class CadastroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        this.btnEntrar = findViewById(R.id.btnEntrar)
+        this.edtNome = findViewById(R.id.etEmail)
+        this.edtSenha = findViewById(R.id.etSenha)
+
+        this.btnEntrar.setOnClickListener (){
+            try{
+                val createUserData = hashMapOf(
+                    "nome" to this.edtNome.text.toString(),
+                    "senha" to  this.edtSenha.text.toString()
+                )
+
+                db.collection("Usuario")
+                    .add(createUserData)
+                    .addOnSuccessListener { documentReference ->
+                        // O documento foi adicionado com sucesso, e documentReference contém o ID gerado automaticamente.
+                        val novoDocumentoId = documentReference.id
+                    }
+            }
+            catch (e: Exception){
+                val mensagem = "Você clicou no botão!"
+                val duracao = Toast.LENGTH_SHORT // ou Toast.LENGTH_LONG
+
+                val toast = Toast.makeText(applicationContext, mensagem, duracao)
+                toast.show()
+            }
+        }
     }
 }
