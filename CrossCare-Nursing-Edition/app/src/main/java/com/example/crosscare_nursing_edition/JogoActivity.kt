3b4809e45ee5d7c68b8a4135084fdfe06325c5cd
Maria Eduarda Padovan <107715022+PADOVSZ@users.noreply.github.com>
2023-10-20 08:09:48 -0300

    Atualização 19/10

---
+++ CrossCare-Nursing-Edition/app/src/main/java/com/example/crosscare_nursing_edition/JogoActivity.kt
@@ -0,0 +1,18 @@
+package com.example.crosscare_nursing_edition
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.crosscare_nursing_edition.databinding.JogoBinding
+
+class JogoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.jogo)
+    }
+}
