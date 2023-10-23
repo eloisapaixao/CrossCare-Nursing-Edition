f7bbc83b864595a37896c1a7c518d2d03e78c068
Eloisa Paixão <114162946+eloisapaixao@users.noreply.github.com>
2023-10-23 10:26:34 -0300

    Atualização 23/10/2023
    Cadastro funcionando!!!!!!!!

--- CrossCare-Nursing-Edition/app/src/main/java/com/example/crosscare_nursing_edition/CadastroActivity.kt
+++ CrossCare-Nursing-Edition/app/src/main/java/com/example/crosscare_nursing_edition/CadastroActivity.kt
@@ -8,14 +8,15 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
-import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 
 class CadastroActivity : AppCompatActivity() {
     lateinit var btnEntrar: Button
-    lateinit var edtNome: EditText
+    lateinit var edtEmail: EditText
     lateinit var edtSenha: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,31 +31,33 @@ class CadastroActivity : AppCompatActivity() {
         )
 
         this.btnEntrar = findViewById(R.id.btnEntrar)
-        this.edtNome = findViewById(R.id.etEmail)
+        this.edtEmail = findViewById(R.id.etEmail)
         this.edtSenha = findViewById(R.id.etSenha)
 
-        this.btnEntrar.setOnClickListener (){
+        this.btnEntrar.setOnClickListener(){
             try{
                 val url = "http://192.168.107.46:3000/cadastro"
+                val requestQueue = Volley.newRequestQueue(this)
+                val jsonObject = JSONObject()
+                jsonObject.put("email", edtEmail.text.toString())
+                jsonObject.put("senha", edtSenha.text.toString())
 
-                val queue: RequestQueue = MyVolley.getRequestQueue()
-                
-                val jsonObjectRequest = JsonObjectRequest(
-                    Request.Method.GET, url, null,
-                    Response.Listener { response ->
+                val jsonObjectRequest = JsonObjectRequest(Request.Method.POST,
+                    url,
+                    jsonObject,
+                    { response ->
                         val intent = Intent(this, JogoActivity::class.java)
                         startActivity(intent)
                     },
-                    Response.ErrorListener { error ->
-                        // TODO: Handle error
-                    }
-                )
+                    { error ->//Erro ao coletar os dados!
+                        val toast = Toast.makeText(applicationContext, "Erro: ${error.message}", Toast.LENGTH_LONG)
+                        toast.show()
+                    })
 
-                // Access the RequestQueue through your singleton class.
-                MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+                requestQueue.add(jsonObjectRequest)
             }
             catch (e: Exception){
-                val mensagem = "Você clicou no botão!"
+                val mensagem = "Erro ao cadastrar!"
                 val duracao = Toast.LENGTH_SHORT // ou Toast.LENGTH_LONG
 
                 val toast = Toast.makeText(applicationContext, mensagem, duracao)
